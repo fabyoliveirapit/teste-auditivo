2b207351107c04ac9dea4d7b85b5cfb55a854bd2
fabyoliveirapit <fabryciathais@gmail.com>
2025-06-17 01:59:06 -0300

    Create meu_scripts.js

---
+++ dataincludes/meu_scripts.js
@@ -0,0 +1,21 @@
+PennController.ResetPrefix(null)
+
+Sequence("Participante","Instrucoes", randomize("Experimento"),SendResults(), "Final";
+    newButton("Hello World")
+Header(
+        
+defaultText
+        .css("font-size","1.2en")
+        .print(
+        ,
+    
+defaultTextInput
+        .css("fonte-size", "1.2en")
+        .print()
+        ,
+        
+defaultButton
+        .css("fonte-size", "1.2en")
+        .print()
+        .center()
+        .wait()
